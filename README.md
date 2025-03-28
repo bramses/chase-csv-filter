4daf4245c7f88b85903e5a79f11db36eefc624b0
Bram Adams <3282661+bramses@users.noreply.github.com>
2025-03-28 11:10:07 -0400

    init

---
+++ README.md
@@ -0,0 +1,10 @@
+filter chase transactions using an array of descriptions.
+tabulate total yearly cost.
+
+
+
+to run: 
+1. download transactions as csv from chase
+2. move to this folder
+3. npm install
+4. node filter_csv.js input.csv output.csv
---
+++ filter_csv.js
@@ -0,0 +1,46 @@
+// Bram Adams (bramadams.dev) 2025
+// row headers (for Chase Credit Card): Transaction Date	Post Date	Description	Category	Type	Amount	Memo
+// to run: node filter_csv.js input.csv output.csv
+
+
+const fs = require('fs');
+const csv = require('fast-csv');
+const dotenv = require('dotenv')
+
+dotenv.config()
+
+const inputFile = process.argv[2] || 'input.csv';
+const outputFile = process.argv[3] || 'filtered.csv';
+
+
+// Keywords to filter by (e.g., SUPABASE, SQSP, etc.)
+const keywords = JSON.parse(process.env.KEYWORDS);
+// Read and filter CSV
+const results = [];
+
+let isFirstRow = true;
+let total = 0;
+
+fs.createReadStream(inputFile)
+  .pipe(csv.parse({ headers: false, delimiter: '\t' })) // Adjust delimiter if needed
+  .on('data', (row) => {
+    if (isFirstRow) {
+      isFirstRow = false; // Skip the first row
+      return;
+    }
+    const delimterRow = row[0].split(',');
+    
+    const searchField = delimterRow[2]; // Assuming the vendor name is in column 3 (index 2)
+    if (keywords.some(keyword => searchField.includes(keyword))) {
+      results.push(delimterRow);
+      total += Number(delimterRow[5])
+    }
+  })
+  .on('end', () => {
+    // Write filtered results
+    const ws = fs.createWriteStream(outputFile);
+    csv.write(results, { headers: false, delimiter: '\t' }).pipe(ws);
+    console.log(`Filtered CSV saved to ${outputFile}`);
+    console.log(`Total amount spent: ${total}`)
+  });
+
